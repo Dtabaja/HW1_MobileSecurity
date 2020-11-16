0981ebe62be59b70a8c8a247bb68f47860310292
Daniel Tabaja <dtabaja@gmail.com>
2020-11-16 18:28:59 +0200

    MAX Volume added

--- app/src/main/java/com/example/myapplication/LoginSuccessful.java
+++ app/src/main/java/com/example/myapplication/LoginSuccessful.java
@@ -1,4 +1,5 @@
 package com.example.myapplication;
+
 import android.os.Bundle;
 import android.view.WindowManager;
 
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
@@ -18,10 +19,8 @@ import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-// Variables for phone's button.
-private MaterialButton main_BTN_click;
+    // Variables for phone's button.
+    private MaterialButton main_BTN_click;
 
     // Variable for sensors.
     private SensorManager sensorManager;
@@ -35,18 +34,21 @@ private MaterialButton main_BTN_click;
 
     // Variable for tilt sensor.
     private double xTilt = 0;
-    private float[] gData = new float[3];
 
     // Variables for step counter sensor.
     private float stepCounter = 0, initialStepCounter = 0;
     private boolean firstStep = true;
+
+    // Variables for phone's Volume sound's case.
+    private AudioManager audio;
+    private int currentVolume;
+    private int maxVolume;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         main_BTN_click = findViewById(R.id.main_BTN_click);
@@ -60,7 +62,6 @@ private MaterialButton main_BTN_click;
             public void onSensorChanged(SensorEvent sensorEvent) {
                 if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
                     xTilt = sensorEvent.values[0];
-                    gData = sensorEvent.values.clone();
                 }
                 if (sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
                     if (firstStep) {
@@ -70,6 +71,7 @@ private MaterialButton main_BTN_click;
                     stepCounter = sensorEvent.values[0] - initialStepCounter;
                 }
             }
+
             @Override
             public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
@@ -77,37 +79,59 @@ private MaterialButton main_BTN_click;
 
         };
 
-main_BTN_click.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        registerTheTilt();
+        main_BTN_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        // If the bright or the  is on the max level it will sign in or
-        // if  the phone in tilt to the left or
-        // If you make more then 5 steps.
-        try {
-            currentBrigthLight = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS);
-        } catch (Settings.SettingNotFoundException e) {
-            e.printStackTrace();
-        }
-        if(currentBrigthLight == MAX_LIGHT|| (xTilt > 3 && xTilt < 10 || stepCounter > 5)){
-            Toast.makeText(MainActivity.this, "Login Successfully!", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(MainActivity.this, LoginSuccessful.class);
-            startActivity(intent);
+                registerTheSensors();
+                audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
+                maxVolume = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
 
-        }
+                try {
+                    currentBrigthLight = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS);
+                } catch (Settings.SettingNotFoundException e) {
+                    e.printStackTrace();
+                }
+                //If the bright or the  is on the max level it will sign in.
+                if (currentBrigthLight == MAX_LIGHT) {
+                    Toast.makeText(MainActivity.this, "Login Successfully-MaxLight!", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, LoginSuccessful.class);
+                    startActivity(intent);
+                }
 
+                //If  the phone in tilt to the left.
+                else if (xTilt > 3 && xTilt < 10) {
+                    Toast.makeText(MainActivity.this, "Login Successfully- tilt!", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, LoginSuccessful.class);
+                    startActivity(intent);
+                }
 
-        else{
-            Toast.makeText(MainActivity.this,"Login failed!", Toast.LENGTH_LONG).show();
+                //If you make more then 5 steps.
+                else if (stepCounter > 5) {
+                    Toast.makeText(MainActivity.this, "Login Successfully-steps!", Toast.LENGTH_LONG).show();
+                    stepCounter = 0;
+                    Intent intent = new Intent(MainActivity.this, LoginSuccessful.class);
+                    startActivity(intent);
+                }
 
-        }
+                //If your volume sound level is at max.
+                else if (currentVolume == maxVolume) {
+                    Toast.makeText(MainActivity.this, "Login Successfully-MaxVolume!", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, LoginSuccessful.class);
+                    startActivity(intent);
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Login failed!", Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
 
     }
-});
 
-    }
-    private void registerTheTilt(){
+    private void registerTheSensors() {
         sensorManager.registerListener(sensorEventListener, tiltSensor, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(sensorEventListener, stepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
